38aa86883799019a62a768b08d09c7f6cb62f789
suniljnvs <sunilkumar0551998@gmail.com>
2022-04-11 19:32:03 +0530

    player assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
@@ -7,6 +8,13 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://sunil-1998:xUgDvP9q5zTG5ZBT@cluster0.7uvij.mongodb.net/sunil1998", {
+    useNewUrlParser: true
+
+})
+
+.then(() => console.log("mongo DB is connected"))
+    .catch(err => console.log(err))
 
 app.use('/', route);
 
