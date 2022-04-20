95f5202ee0e51e934cf664f8d5ca3e334b1b81b3
suniljnvs <sunilkumar0551998@gmail.com>
2022-04-20 12:03:15 +0530

    middlewere

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+const route = require('./routes/route');
 const { default: mongoose } = require('mongoose');
+const moment = require('moment')
+
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+mongoose.connect("mongodb+srv://Nil05:RTFSM7DZfy6AZPkA@cluster0.ixogq.mongodb.net/Nil", {
+        useNewUrlParser: true
+    })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use(
+    function(req, res, next) {
+        let data = moment().format("YYYY-MM-DD  HH:MM:SS")
+        console.log(data + "," + req.ip + "," + req.url)
+        next()
+    }
+
+);
 
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
+
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
